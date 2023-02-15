f42b1b30ec39bb36a78ea7f70e063093a6fcdeaf
Raffael28 <rafael.oliveira.aut18@gmail.com>
2023-02-15 11:01:53 -0300

    Create  generic Files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import styles from "./assets/App.module.css";
+import styles from "./App.module.css";
 
 function App() {
   const [count, setCount] = useState(0);
