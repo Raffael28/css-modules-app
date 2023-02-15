d1bd66a976c5883f4827344e967601f24b1213cd
Raffael28 <rafael.oliveira.aut18@gmail.com>
2023-02-15 11:05:32 -0300

    import components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
 import styles from "./App.module.css";
+import Background from "./Background/Background";
+import Grid from "./Grid/Grid";
+import Rocket from "./Rocket/Rocket";
+import Smoke from "./Rocket/Smoke";
+import Title from "./Title/Title";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className={styles.App}></div>;
+  return (
+    <div className={styles.App}>
+      <Background />
+      <Grid />
+      <Rocket />
+      <Smoke />
+      <Title />
+    </div>
+  );
 }
 
 export default App;
